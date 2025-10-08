cbd3f271d4953be7aefc5cb71e4f1119699a9fe9
galaks <narot.zhora@mail.ru>
2025-10-08 18:30:15 +0300

    сделал тултип

--- src/components/SidebarLayout.tsx
+++ src/components/SidebarLayout.tsx
@@ -1,6 +1,9 @@
 import { Link, Outlet } from "react-router-dom";
+import { useState } from "react";
 
 const SidebarLayout: React.FC = () => {
+  const [showTooltip, setShowTooltip] = useState(false);
+
   return (
     <div className="flex h-screen">
       <aside className="w-52 bg-blue-900 p-4 flex flex-col justify-between">
@@ -17,17 +20,15 @@ const SidebarLayout: React.FC = () => {
             </ul>
           </nav>
         </div>
-        <div className="">
-          <Link to={"profile/id"}>
-            <div className="flex items-center relative">
+        <Link to={"profile/id"}>
+          <div className="relative mb-20">
+            <div onMouseEnter={() => setShowTooltip(true)} onMouseLeave={() => setShowTooltip(false)} className="flex items-center">
               <img src="/user-icon.png" alt="" className="w-7" />
               <h3 className="ml-1">Иван (Инженер)</h3>
-              {/* <div className="absolute -top-8 left-1/2 transform -translate-x-1/2 bg-gray-800 text-white text-xs px-2 py-1 rounded opacity-0 group-hover:opacity-100 transition-opacity duration-200 whitespace-nowrap pointer-events-none">
-                Профиль
-              </div> */}
             </div>
-          </Link>
-        </div>
+            <div className={`absolute top-full left-1/2 transform -translate-x-1/2 mt-1 bg-gray-700 text-white text-xs rounded px-2 py-1 shadow-lg whitespace-nowrap z-10 transition-opacity duration-300 ${showTooltip ? "opacity-100" : "opacity-0"}`}>Профиль</div>
+          </div>
+        </Link>
       </aside>
       <main className="flex-1 bg-gray-800 p-1">
         <Outlet />
