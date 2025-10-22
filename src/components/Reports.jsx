e10db5a3cd77b7829df667c79c3119184e8a909e
galaks <narot.zhora@mail.ru>
2025-10-22 18:44:49 +0300

    тесты литкод

--- src/components/Reports.jsx
+++ src/components/Reports.jsx
@@ -1,6 +1,43 @@
 import React from 'react'
 
-const ReportsPage: React.FC = () => {
+const ReportsPage = () => {
+
+
+
+
+
+
+
+var maxProfit = function(prices) {
+    let i = 0
+    if (prices[i] <= prices[i+1]){
+        if (prices[i] <= prices[i+2]) {
+          
+        }
+    }
+        
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   return (
     <div className='p-4'>
       <h1 className='text-2xl font-bold mb-4'>Отчеты</h1>
